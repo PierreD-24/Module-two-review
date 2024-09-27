958dd82f5c26c2765deb56b8f0456e0c0d6ffe7d
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-27 12:19:14 -0700

    initial commit completed student class

---
+++ Module_2_Exercise/Student.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+public class Student
+{
+    public string? Name { get; set; }
+    public int ID { get; set; }
+    public List<double> Grades { get; set; } = new List<double>();
+
+    public void AddGrade(double grade)
+    {
+        Grades.Add(grade);
+    }
+
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
+    }
+}
