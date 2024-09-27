14b6c876926685d127254d9db508bb9d5b553a6c
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-27 12:56:41 -0700

    third commit worked on main function created students

--- Module_2_Exercise/Program.cs
+++ Module_2_Exercise/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ModuleReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            
+            Student firstStudent = new Student() {Name = "Michael",ID = 24};
+            Student secondStudent = new Student() {Name = "Megan", ID = 42};
+            Student thirdStudent = new Student() {Name = "Jimmy", ID = 12};
+            Student fourthStudent = new Student() {Name = "Jason", ID = 19};
+
+           students.Add(firstStudent);
+           students.Add(secondStudent);
+           students.Add(thirdStudent);
+           students.Add(fourthStudent);
+
+        }
+    }
+}
